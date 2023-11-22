bb34aa24e5b310a295d4e290835f918af7e3a228
Ommo <45515626+Ommooo3@users.noreply.github.com>
2023-11-22 20:23:49 +0000

    Add files via upload

--- JS/overlays.js
+++ JS/overlays.js
@@ -1,44 +1,4 @@
-function toggleOverlay1() {
-  var overlay = document.getElementsByClassName("overlay-container1")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay2() {
-  var overlay = document.getElementsByClassName("overlay-container2")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay3() {
-  var overlay = document.getElementsByClassName("overlay-container3")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay4() {
-  var overlay = document.getElementsByClassName("overlay-container4")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay5() {
-  var overlay = document.getElementsByClassName("overlay-container5")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay6() {
-  var overlay = document.getElementsByClassName("overlay-container6")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay7() {
-  var overlay = document.getElementsByClassName("overlay-container7")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay8() {
-  var overlay = document.getElementsByClassName("overlay-container8")[0];
-  overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
-}
-
-function toggleOverlay9() {
-  var overlay = document.getElementsByClassName("overlay-container9")[0];
+function toggleOverlay() {
+  var overlay = document.getElementsByClassName("overlay-container")[0];
   overlay.style.display = (overlay.style.display === "flex") ? "none" : "flex";
 }
