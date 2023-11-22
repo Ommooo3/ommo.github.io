8dff93438f3297e8322169fbf570ef7c332b108c
Ommo <45515626+Ommooo3@users.noreply.github.com>
2023-11-22 14:19:30 +0000

    Add files via upload

--- JS/scroll.js
+++ JS/scroll.js
@@ -6,3 +6,14 @@ function scrollToAbout()
         behavior: 'smooth'
     })
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
