8c04e9e4b13c6d2b498d14502dc6abdb2d884f02
Ommo <45515626+Ommooo3@users.noreply.github.com>
2023-12-07 14:08:10 +0000

    Add files via upload

--- JS/overlays.js
+++
@@ -1,7 +0,0 @@
-function on() {
-  document.getElementsByClassName("overlay").style.cssText = "display: block;"; 
-}
-
-function off() {
-  document.getElementsByClassName("overlay").style.cssText = "display: none;"; 
-}
