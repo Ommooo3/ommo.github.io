56f4353f35d54b763e2ec9906cba8d81073fcc24
Ommo <45515626+Ommooo3@users.noreply.github.com>
2023-12-13 14:17:41 +0000

    Add files via upload

--- JS/overlays.js
+++ JS/overlays.js
@@ -1,15 +1,34 @@
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+$(document).ready(function () {
+var player = $('#cp2077')[0];
+  var player2 = $('#ommologos')[0];
+ $f(player).addEvent('ready', ready); 
+  $f(player2).addEvent('ready', ready); 
+  
+  function addEvent(element, eventName, callback) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, callback, false);
+    }
+    else {
+      element.attachEvent(eventName, callback, false);
+    }
+  }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+  function ready(player_id) {
+    var froogaloop = $f(player_id);
 
-function on2() {
-  document.getElementById("overlay").style.display = "block";
-}
+    function onFinish() {
+      froogaloop.addEvent('finish', function(data) {
+          toggleOverlay(player_id);
+          resetVideo(player_id);
+      });
+    }
+    onFinish();
+  }
 
-function off2() {
-  document.getElementById("overlay").style.display = "none";
+});
+
+function toggleOverlay(playerid){
+   $("#" + playerid).parent("#specialBox").parent().css("opacity",".8");
+   $("#" + playerid).parent("#specialBox").parent().toggle();
+   $("#" + playerid).parent("#specialBox").toggle();
 }
